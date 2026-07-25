7e3b90e1487e1f4985c7ef325243f791904a8642
bayzidMostafiz <bayazid416@gmail.com>
2026-07-25 17:52:45 +0600

    10 tasks completed

---
+++ script.js
@@ -0,0 +1,82 @@
+// Task 1
+console.log("Task 1");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i, "Hello");
+}
+// Task 2
+console.log("Task 2");
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Value of I: ", i)
+}
+
+// Task 3
+console.log("Task 3")
+
+for (let i = 10; i >= 1; i--) {
+    console.log("Value of I: ", i)
+}
+
+//Task 4
+console.log("Task 4")
+
+let i = 1;
+while (i <= 7) {
+    console.log(i, "Loop চলছে");
+    i++;
+}
+
+//Task 5
+console.log("Task 5")
+
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+// Task 6
+console.log("Task 6")
+
+let sum = 0;
+for (let i = 1; i <= 20; i++) {
+    sum = sum + i;
+    console.log(sum)
+}
+
+// Task 7
+console.log("Task 7")
+
+let number = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(number, "X", i, "=", number * i)
+}
+
+// Task 8
+console.log("Task 8")
+
+for (i = 20; i >= 1; i--) {
+    console.log(i)
+}
+
+// Task 9
+console.log("Task 9")
+
+let sum = 0;
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        sum = sum + i;
+        console.log(sum)
+    }
+}
+
+// Task 10
+console.log("Task 10");
+
+for (let i = 1; i <= 50; i++){
+    console.log(i);
+    if(i >=30){
+        break
+    }
+}
